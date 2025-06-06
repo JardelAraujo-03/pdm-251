f0ec94594e19c87a6670c5ea463d5ec4d6a1ba29
JardelAraujo-03 <jardel.araujo03@aluno.ifce.edu.br>
2025-06-05 22:49:13 -0300

    Criada em 05.06.2025

---
+++ Avaliações/Prova-Pratica-01/prova-pratica-01/bin/prova_pratica_01.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+
+class Cliente {
+  int codigo;
+  String nome;
+  int tipoCliente;
+
+  Cliente(this.codigo, this.nome, this.tipoCliente);
+
+  Map<String, dynamic> toJson() =>
+      {'codigo': codigo, 'nome': nome, 'tipoCliente': tipoCliente};
+}
+
+class Vendedor {
+  int codigo;
+  String nome;
+  double comissao;
+
+  Vendedor(this.codigo, this.nome, this.comissao);
+
+  Map<String, dynamic> toJson() =>
+      {'codigo': codigo, 'nome': nome, 'comissao': comissao};
+}
+
+class Veiculo {
+  int codigo;
+  String descricao;
+  double valor;
+
+  Veiculo(this.codigo, this.descricao, this.valor);
+
+  Map<String, dynamic> toJson() =>
+      {'codigo': codigo, 'descricao': descricao, 'valor': valor};
+}
+
+class ItemPedido {
+  int sequencial;
+  String descricao;
+  int quantidade;
+  double valor;
+
+  ItemPedido(this.sequencial, this.descricao, this.quantidade, this.valor);
+
+  Map<String, dynamic> toJson() => {
+        'sequencial': sequencial,
+        'descricao': descricao,
+        'quantidade': quantidade,
+        'valor': valor
+      };
+}
+
+class PedidoVenda {
+  String codigo;
+  DateTime data;
+  Cliente cliente;
+  Vendedor vendedor;
+  Veiculo veiculo;
+  List<ItemPedido> items;
+
+  PedidoVenda(this.codigo, this.data, this.cliente, this.vendedor,
+      this.veiculo, this.items);
+
+  double calcularPedido() =>
+      items.fold(0, (total, item) => total + item.valor * item.quantidade);
+
+  Map<String, dynamic> toJson() => {
+        'codigo': codigo,
+        'data': data.toIso8601String(),
+        'cliente': cliente.toJson(),
+        'vendedor': vendedor.toJson(),
+        'veiculo': veiculo.toJson(),
+        'items': items.map((e) => e.toJson()).toList()
+      };
+}
+
+void main() async {
+  var cliente = Cliente(1, 'João da Silva', 2);
+  var vendedor = Vendedor(101, 'Maria Oliveira', 0.05);
+  var veiculo = Veiculo(501, 'Carro Sedan', 45000.0);
+
+  var item1 = ItemPedido(1, 'Rodas esportivas', 2, 1500.0);
+  var item2 = ItemPedido(2, 'Som automotivo', 1, 2000.0);
+
+  var pedido = PedidoVenda(
+    'P001',
+    DateTime.now(),
+    cliente,
+    vendedor,
+    veiculo,
+    [item1, item2],
+  );
+
+  String jsonPedido = jsonEncode(pedido.toJson());
+  final jsonFile = File('pedido.json');
+  await jsonFile.writeAsString(jsonPedido);
+  print('✅ Arquivo pedido.json criado!');
+
+  final String username = 'jardel.araujo03@aluno.ifce.edu.br';
+  final String password = 'htzl mlhb zpkk lqdn'; 
+  final smtpServer = gmail(username, password);
+
+  final attachment = FileAttachment(jsonFile)
+    ..location = Location.inline
+    ..cid = '<pedido.json>';
+
+  final message = Message()
+    ..from = Address(username, 'Jardel Araujo')
+    ..recipients.add('jardel.araujo03@aluno.ifce.edu.br')
+    ..subject = 'Arquivo JSON do Pedido'
+    ..text = '''
+Segue em anexo o arquivo JSON gerado pelo sistema.
+
+Resumo:
+Código do Pedido: ${pedido.codigo}
+Cliente: ${cliente.nome}
+Valor Total: R\$ ${pedido.calcularPedido().toStringAsFixed(2)}
+
+Atenciosamente,
+Sistema de Revenda
+'''
+    ..attachments = [attachment];
+
+  try {
+    final sendReport = await send(message, smtpServer);
+    print('📧 E-mail enviado com sucesso!');
+    print('Detalhes do envio: $sendReport');
+  } on MailerException catch (e) {
+    print('❌ Falha ao enviar e-mail: ${e.toString()}');
+  }
+}
